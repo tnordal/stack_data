6213ada60ce98b8318eea08a6ebbd8014d90606a
Tom Nordal <tnordal@gmail.com>
2021-11-13 09:23:44 +0100

    add fields

--- models/bar.py
+++ models/bar.py
@@ -1,3 +1,23 @@
 class Bar:
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        ticker: str,
+        timestamp: int,
+        open: float,
+        high: float,
+        low: float,
+        close: float,
+        adj_close: float,
+        volume: int,
+        _id: int = None
+    ) -> None:
+        self.id = _id
+        self.ticker = ticker
+        self.date = timestamp
+        self.open = open
+        self.high = high
+        self.low = low
+        self.close = close
+        self.adj_close = adj_close
+        self.volume = volume
+
