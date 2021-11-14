f0d98e6ad2ca7a096b643fa815923774b3dd9f71
Tom Nordal <tnordal@gmail.com>
2021-11-14 15:51:26 +0100

    Update single ticker price data

--- app_cli.py
+++ app_cli.py
@@ -1,29 +1,61 @@
+import download
+from connection_pool import get_connection
+import database
+
+
 MAIN_MENU_PROMPT = """
 --- Main Menu ---
 
-1) Menu 1
-2) Menu 2
+1) Update price data (bars)
+2) Update single ticker price data
 3) Menu 3
 q) Exit Main Menu
 
 Enter your choise:"""
 
-
-def menu_1():
-    print('Menu 1')
+# --- Menu Prompts ---
 
 
-def menu_2():
-    print('Menu 2')
+def update_bars_promt():
+    exchange = input('Enter Exchange (Oslo, Sp500, Stockholm):')
+    period = input('Enter period (1y, 2y...):')
+    print(f"Update {exchange} for period of {period}")
+
+
+def update_ticker_prompt():
+    ticker = input('Enter ticker:')
+    period = input('Enter period (1y, 2y...):')
+    update_ticker(ticker=ticker, period=period)
 
 
 def menu_3():
     print('Menu 3')
 
+# --- Menu Functions ---
+
+
+def update_ticker(ticker, period):
+    print(f"Update {ticker} for period of {period}")
+    df = download.download_history(ticker, period)
+    df = download.filter_data_by_ts(df, ticker)
+    bulk_insert(df, 'bars')
+
+
+def bulk_insert(df, table):
+    if len(df) > 0:
+        with get_connection() as connection:
+            ret = database.bulk_insert_bars(connection, df, table)
+            if ret is True:
+                print(f"Inserted {len(df)} rows!")
+            else:
+                print(ret)
+    else:
+        print('Nothing to insert!')
+
 
 MAIN_MENU_OPTIONS = {
-    '1': menu_1,
-    '2': menu_2,
+    '1': update_bars_promt,
+    '2': update_ticker_prompt,
     '3': menu_3
 }
 
