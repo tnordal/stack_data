faef04a58cb09f425789a7362d9d828fac15bbc1
Tom Nordal <tnordal@gmail.com>
2021-11-14 07:23:11 +0100

    removed user input

--- connection_pool.py
+++ connection_pool.py
@@ -3,13 +3,13 @@ from contextlib import contextmanager
 from psycopg2.pool import SimpleConnectionPool
 from dotenv import load_dotenv
 
-DATABASE_PROMPT = \
-    "Enter the DATABASE_URI value or leave empty to load from .env file: "
+# DATABASE_PROMPT = \
+#     "Enter the DATABASE_URI value or leave empty to load from .env file: "
 
-database_uri = input(DATABASE_PROMPT)
-if not database_uri:
-    load_dotenv()
-    database_uri = os.environ["DATABASE_URI"]
+# database_uri = input(DATABASE_PROMPT)
+# if not database_uri:
+load_dotenv()
+database_uri = os.environ["DATABASE_URI"]
 
 pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=database_uri)
 
