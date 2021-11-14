1cd10178e1a7453dd1d36332f3b709695ef232a4
Tom Nordal <tnordal@gmail.com>
2021-11-14 07:24:49 +0100

    add bulk_insert_bars first version

--- database.py
+++ database.py
@@ -1,4 +1,11 @@
+# import os
+from io import StringIO
+# import pandas as pd
+
 from contextlib import contextmanager
+from psycopg2.errorcodes import UNIQUE_VIOLATION
+from psycopg2 import errors
+
 
 # --- Reset Database ---
 DROP_TABLE_COMPANIES = "DROP TABLE IF EXISTS companies;"
@@ -16,13 +23,14 @@ CREATE_BARS = """
     CREATE TABLE IF NOT EXISTS bars (
         id SERIAL PRIMARY KEY,
         ticker TEXT,
-        timestamp REAL,
+        ts REAL,
         open NUMERIC,
         high NUMERIC,
         low NUMERIC,
         close NUMERIC,
         adj_close NUMERIC,
-        volume BIGINT
+        volume BIGINT,
+        UNIQUE (ticker, ts)
     );
 """
 
@@ -46,8 +54,24 @@ def drop_tables(connection):
         cursor.execute(DROP_TABLE_BARS)
 
 
+def bulk_insert_bars(connection, df):
+    # Initialize a string buffer
+    sio = StringIO()
+    # Write the Pandas DataFrame as a csv to the buffer
+    sio.write(df.to_csv(index=None, header=None))
+    # Be sure to reset the position to the start of the stream
+    sio.seek(0)
+
+    with get_cursor(connection) as cursor:
+        try:
+            cursor.copy_from(sio, 'bars', columns=df.columns, sep=',')
+            connection.commit()
+        except errors.lookup(UNIQUE_VIOLATION):
+            print('Error: Rows already exists!')
+
+
 if __name__ == '__main__':
     from connection_pool import get_connection
     with get_connection() as connection:
-        # create_tables(connection)
-        drop_tables(connection)
+        create_tables(connection)
+        # drop_tables(connection)
