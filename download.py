92ea6b90ce052505c2cfc03aa0977ee1cac20f30
Tom Nordal <tnordal@gmail.com>
2021-11-14 13:06:13 +0100

    add prepare companie files and testing add companies to db

--- download.py
+++ download.py
@@ -34,22 +34,69 @@ def download_history(ticker, period='1y'):
     return df
 
 
-def filter_data_by_ts(df):
+def filter_data_by_ts(df, ticker):
     with get_connection() as connection:
-        last_ts = database.get_last_ts(connection, 'NHY.OL')
-        first_ts = database.get_first_ts(connection, 'NHY.OL')
+        last_ts = database.get_last_ts(connection, ticker)
+        first_ts = database.get_first_ts(connection, ticker)
     if last_ts is None:
         return df
     mask = (df['date'] > last_ts) | (df['date'] < first_ts)
     return df[mask]
 
 
+def prepare_companies_file_for_db(
+    ticker_file, ticker_column='ticker',
+    name_column='Company', sector_column='sector',
+    exchange='Oslo'
+):
+    df_raw = pd.read_csv(ticker_file)
+    df_db = pd.DataFrame(columns=['ticker', 'name', 'exchange', 'sector'])
+    df_db['ticker'] = df_raw[ticker_column]
+    df_db['name'] = df_raw[name_column].str.replace(',', '')
+    df_db['exchange'] = exchange
+    df_db['sector'] = df_raw[sector_column].str.replace(',', '')
+    # Remove dublicates tickers, is a primary key
+    df_db = df_db.drop_duplicates(subset=['ticker'])
+    return df_db
+
+
 if __name__ == '__main__':
-    df = download_history('NHY.OL')
-    df = filter_data_by_ts(df)
+    # Oslo.csv
+    df = prepare_companies_file_for_db(
+        ticker_file='data_files/companies/oslo.csv',
+        ticker_column='Ticker',
+        name_column='Company',
+        sector_column='GICS Industry[5]',
+        exchange='Oslo'
+    )
+
+    # # SP500.csv
+    # df = prepare_companies_file_for_db(
+    #     ticker_file='data_files/companies/sp500.csv',
+    #     ticker_column='Ticker',
+    #     name_column='Security',
+    #     sector_column='GICS Sector',
+    #     exchange='Sp500'
+    # )
+
+    # # stockholm.csv
+    # df = prepare_companies_file_for_db(
+    #     ticker_file='data_files/companies/stockholm.csv',
+    #     ticker_column='Ticker',
+    #     name_column='Company Name',
+    #     sector_column='Sector',
+    #     exchange='Stockholm'
+    # )
+
+    # df = download_history('NHY.OL')
+    # df = filter_data_by_ts(df, ticker='NHY.OL')
+
     if len(df) > 0:
         with get_connection() as connection:
-            database.bulk_insert_bars(connection, df)
-        print(f"Inserted {len(df)} rows!")
+            ret = database.bulk_insert_bars(connection, df, 'companies')
+            if ret is True:
+                print(f"Inserted {len(df)} rows!")
+            else:
+                print(ret)
     else:
         print('Nothing to insert!')
